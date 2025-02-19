aa118fb98b851b546e6fb1ac517f83d5af7dcf14
Danielip1705 <daniel.iglesias@iesnervion.es>
2025-02-19 17:32:13 +0100

    (Update) Proveedores
    Clase proveedores con sus atributos, propiedades y constructores realizados

--- API_ERP/ENT/Proveedores.cs
+++ API_ERP/ENT/Proveedores.cs
@@ -3,14 +3,61 @@
     public class Proveedores
     {
         #region Atributos
-
+        private int idProveedores;
+        private string nombre;
+        private string direccion;
+        private string email;
         #endregion
 
         #region Propiedades
+        public int IdProveedores
+        {
+            get { return idProveedores; }
+        }
 
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
         #endregion
 
         #region Constructores
+        /// <summary>
+        /// Constructor vacio
+        /// Pre: Nada
+        /// Post: Objeto proveedor con atributos
+        /// </summary>
+        public Proveedores() { }
+
+        /// <summary>
+        /// Constructor con parametros
+        /// Pre: El usuario añade los atributos del objeto
+        /// Post: Se crea un objeto proveedor con sus atributos
+        /// </summary>
+        /// <param name="idProveedores"></param>
+        /// <param name="nombre"></param>
+        /// <param name="direccion"></param>
+        /// <param name="email"></param>
+        public Proveedores(int idProveedores, string nombre, string direccion, string email)
+        {
+            this.idProveedores = idProveedores;
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.email = email;
+        }
+
 
         #endregion
     }
