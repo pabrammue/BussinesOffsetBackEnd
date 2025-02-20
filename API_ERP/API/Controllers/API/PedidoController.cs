7d1eb6d6365ab784a90312ea185cf1f1669a676a
jlmejorada <145675220+jlmejorada@users.noreply.github.com>
2025-02-20 14:21:34 +0100

    [CREATE] Controlador del pedido
    Se ha creado los estructura de todos los verbos de los pedidos

---
+++ API_ERP/API/Controllers/API/PedidoController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers.API
+{
+    [Route("api/pedido")]
+    [ApiController]
+    public class PedidoController : ControllerBase
+    {
+        // GET: api/pedido
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<String> listadoCompleto = new List<String>();
+            try
+            {
+                //List<String> listadoCompleto = "ClsListadoPersonaBL.ListaPersonasBL()"
+                if (listadoCompleto.Count == 0)
+                {
+                    return NotFound("No se encontraron pedidos.");
+                }
+                return Ok(listadoCompleto);
+            }
+            catch
+            {
+                return StatusCode(500, "Error interno del servidor.");
+            }
+        }
+
+        // GET api/pedido/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            String persona = null;
+            try
+            {
+                //ClsPersona persona = ClsManejadoraPersonaBL.BuscaPersonaBL(id);
+                if (persona == null)
+                {
+                    return NotFound($"No se encontró el pedido con el id: {id}.");
+                }
+                return Ok(persona);
+            }
+            catch
+            {
+                return StatusCode(500, "Error interno del servidor.");
+            }
+        }
+
+        // POST api/pedido
+        [HttpPost]
+        public IActionResult Post([FromBody] ENTPedido pedido)
+        {
+            if (pedido == null)
+            {
+                return BadRequest("Datos de pedido no válidos.");
+            }
+            try
+            {
+                bool guardadoCorrectamente = ClsManejadoraPersonaBL.CreaPersonaBL(persona);
+                if (guardadoCorrectamente)
+                {
+                    return Created("Pedido creado correctamente", pedido);
+                }
+                return BadRequest("No se pudo crear el pedido.");
+            }
+            catch
+            {
+                return StatusCode(500, "Error interno del servidor.");
+            }
+        }
+
+        // PUT api/pedido/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] ENTPedido pedido)
+        {
+            if (pedido == null)
+            {
+                return BadRequest("Datos de pedido no válidos.");
+            }
+            try
+            {
+                bool actualizado = ClsManejadoraPersonaBL.EditaPersonaBL(persona);
+                if (actualizado)
+                {
+                    return Accepted("Se ha modificado correctamente", pedido);
+                }
+                else
+                {
+                    return NotFound($"No se encontró el pedido con ID {pedido.Id} para actualizar.");
+                }
+            }
+            catch
+            {
+                return StatusCode(500, "Error interno del servidor.");
+            }
+        }
+
+        // DELETE api/pedido/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                bool seBorra = ClsManejadoraPersonaBL.BorraPersonaBL(id);
+                if (!seBorra)
+                {
+                    return Accepted("Se ha borrado correctamente");
+                }
+                else
+                {
+                    return NotFound($"No se encontró el pedido con ID {id} para eliminar.");
+                }
+            }
+            catch
+            {
+                return StatusCode(500, "Error interno del servidor.");
+            }
+        }
+    }
+}
---
+++ API_ERP/API/Controllers/API/ProductoController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers.API
+{
+    [Route("api/producto")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        // GET: api/producto
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                List<String> listadoCompleto = ClsListadoPersonaBL.ListaPersonasBL();
+                if (listadoCompleto.Count == 0)
+                {
+                    return NotFound("No se encontraron productos.");
+                }
+                return Ok(listadoCompleto);
+            }
+            catch
+            {
+                return StatusCode(500, "Error interno del servidor.");
+            }
+        }
+
+        // GET api/producto/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            String persona = null;
+            try
+            {
+                //ClsPersona persona = ClsManejadoraPersonaBL.BuscaPersonaBL(id);
+                if (persona == null)
+                {
+                    return NotFound($"No se encontró el producto con el id: {id}.");
+                }
+                return Ok(persona);
+            }
+            catch
+            {
+                return StatusCode(500, "Error interno del servidor.");
+            }
+        }
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
