2050e4a2c8e2d6fbcee8a3ef1db15a7c8557c6a6
Danielip1705 <daniel.iglesias@iesnervion.es>
2025-03-04 12:56:20 +0100

    (new) DTO Productos por id proveedor y categoria

---
+++ API_ERP/ENT/ProductosPorProveedorYCategorias.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ENT
+{
+    public class ProductosPorProveedorYCategorias
+    {
+        #region Atributos
+        private int idProducto;
+        private string nombre;
+        private int porcentajeIVA;
+        private int stock;
+        private int idCategoria;
+        private double precioUnitario;
+        #endregion
+        #region Propiedades
+        public int IdProducto
+        {
+            get { return idProducto; }
+            set { idProducto = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int PorcentajeIVA
+        {
+            get { return porcentajeIVA; }
+            set { porcentajeIVA = value; }
+        }
+
+        public int Stock
+        {
+            get { return stock; }
+            set { stock = value; }
+        }
+
+        public int IdCategoria
+        {
+            get { return idCategoria; }
+            set { idCategoria = value; }
+        }
+
+        public double PrecioUnitario
+        {
+            get { return precioUnitario; }
+            set { precioUnitario = value; }
+        }
+        #endregion
+        #region Constructores
+        public ProductosPorProveedorYCategorias() { }
+
+        public ProductosPorProveedorYCategorias(int idProducto, string nombre, int porcentajeIVA, int stock, int idCategoria, double precioUnitario)
+        {
+            this.idProducto = idProducto;
+            this.nombre = nombre;
+            this.porcentajeIVA = porcentajeIVA;
+            this.stock = stock;
+            this.idCategoria = idCategoria;
+            this.precioUnitario = precioUnitario;
+        }
+
+        #endregion
+
+    }
+}
