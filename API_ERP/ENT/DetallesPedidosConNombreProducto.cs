190236a69b772d604577bf79462ac43ff627a800
Danielip1705 <daniel.iglesias@iesnervion.es>
2025-03-06 12:24:53 +0100

    (new) DetallesPedidosConNombreProducto
    Clase detallesPedido que muestra el nombre del producto

---
+++ API_ERP/ENT/DetallesPedidosConNombreProducto.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT
+{
+    public class DetallesPedidosConNombreProducto
+    {
+        #region Atributos
+        private int idDetalles;
+        private int idPedido;
+        private int idProducto;
+        private decimal precioTotal;
+        private decimal cuotaIva;
+        private decimal precioBruto;
+        private int cantidad;
+        private string nombreProducto;
+        #endregion
+
+        #region Propiedades
+        public int IdDetalles
+        {
+            get { return idDetalles; }
+        }
+
+        public int IdPedido
+        {
+            get { return idPedido; }
+            set { idPedido = value; }
+        }
+
+        public int IdProducto
+        {
+            get { return idProducto; }
+            set { idProducto = value; }
+        }
+
+        public decimal PrecioTotal
+        {
+            get { return precioTotal; }
+            set { precioTotal = value; }
+        }
+
+        public decimal PrecioBruto
+        {
+            get { return precioBruto; }
+            set { precioBruto = value; }
+        }
+        public decimal CuotaIva
+        {
+            get { return cuotaIva; }
+            set { cuotaIva = value; }
+        }
+        public int Cantidad
+        {
+            get { return cantidad; }
+            set { cantidad = value; }
+        }
+        public string NombreProducto
+        {
+            get { return nombreProducto; }
+            set { nombreProducto = value; }
+        }
+        #endregion
+
+        #region Constructores
+
+        /// <summary>
+        /// Constructor sin parametros
+        /// Pre: Nada
+        /// Post: Objeto DetallesPedidosConNombreProducto sin atributos
+        /// </summary>
+        public DetallesPedidosConNombreProducto() { }
+
+        /// <summary>
+        /// Constructor con todos los parámetros
+        /// Pre: Usuario manda datos para crear un objeto DetallesPedidosConNombreProducto
+        /// Post : Objeto DetallesPedidosConNombreProducto con todos los atributos
+        /// </summary>
+        /// <param name="idDetalles">Numero entero que indica el id de detalles del pedido</param>
+        /// <param name="idPedido">Numero entero que indica el id del pedido del detalle</param>
+        /// <param name="idProducto">Numero entero que indica el id del producto</param>
+        /// <param name="precioTotal">Decimal que indica el precio total de los productos</param>
+        /// <param name="cuotaIva">Numero decimal que indica el porcentaje de IVA</param>
+        /// <param name="precioBruto">Numero decimal decimal que indica el precio bruto</param>
+        /// <param name="cantidad">Numero entero que indica la cantidad de productos</param>
+        /// <param name="nombreProducto">Cadena que indica el nombre del producto</param>
+        public DetallesPedidosConNombreProducto(int idDetalles, int idPedido, int idProducto, decimal precioTotal, decimal cuotaIva, decimal precioBruto, int cantidad, string nombreProducto)
+        {
+            this.idDetalles = idDetalles;
+            this.idPedido = idPedido;
+            this.idProducto = idProducto;
+            this.precioTotal = precioTotal;
+            this.cuotaIva = cuotaIva;
+            this.precioBruto = precioBruto;
+            this.cantidad = cantidad;
+            this.nombreProducto = nombreProducto;
+        }
+
+        #endregion
+    }
+}
