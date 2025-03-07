1a54845015131ae50044a4d19f2c304e36bd9ca5
Danielip1705 <daniel.iglesias@iesnervion.es>
2025-03-07 19:42:45 +0100

    [update] PedidosConDetallesProductos comentado

--- API_ERP/ENT/PedidosConDetallesProductos.cs
+++ API_ERP/ENT/PedidosConDetallesProductos.cs
@@ -28,12 +28,23 @@ namespace ENT
         
         }
 
+        /// <summary>
+        /// Constructor con id y lista pedidos
+        /// Pre: El usuario envia el pedido y la lista de productos
+        /// Post: Se crea objeto PedidosConDetallesProductos con atributos
+        /// </summary>
+        /// <param name="pedido">Pedido con nombre del proveedor</param>
+        /// <param name="listaProductos">Listado de detallesPedidos con nombre de los productos</param>
         public PedidosConDetallesProductos(PedidosConNombreProveedor pedido, List<DetallesPedidosConNombreProducto> listaProductos)
         {
             this.pedido = pedido;
             this.listaProductos = listaProductos;
         }
 
+        /// <summary>
+        /// Constructor con pedidos 
+        /// </summary>
+        /// <param name="conjunto">Objeto PedidosConDetallesProductos a copiar.</param>
         public PedidosConDetallesProductos(PedidosConDetallesProductos conjunto)
         {
             this.pedido = conjunto.pedido;
