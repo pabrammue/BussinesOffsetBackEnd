72db6b35fb29699967d6955d7ff9fae3c7818bbd
pCarbonero <pablo.carbonero@iesnervion.es>
2025-03-09 20:37:35 +0100

    intento 4

--- API_ERP/API/Program.cs
+++ API_ERP/API/Program.cs
@@ -1,33 +1,52 @@
 var builder = WebApplication.CreateBuilder(args);
 
+////////////////
+
 // Configurar CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200", "https://kind-wave-047b9021e.6.azurestaticapps.net")
+            policy.WithOrigins("http://localhost:4200", "https://kind-wave-047b9021e.6.azurestaticapps.net") // Permitir Angular
                   .AllowAnyMethod()
                   .AllowAnyHeader();
         });
 });
 
-// Agregar controladores
+
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Middleware de redirección HTTPS y archivos estáticos
+
+// Usar CORS antes de los controladores
+app.UseCors("AllowAngular");
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
+
+
+///////////////
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting(); // ?? Primero Routing
+app.UseRouting();
 
-app.UseCors("AllowAngular"); // ?? Luego CORS, antes de Authorization
+app.UseAuthorization(); // se pone aqui otra vez por ejemplo
 
-app.UseAuthorization(); // ?? Ahora sí, Authorization
-
-app.MapControllers();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
